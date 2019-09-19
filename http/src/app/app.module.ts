c9d339c93151510a701b5b87dc74944750fff6de
lucalabarbera <lucalabarbera@icloud.com>
2019-09-19 06:52:29 +0000

    http lezione 1

---
+++ http/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooComponent } from './foo/foo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooComponent
+  ],
+  imports: [
+    BrowserModule,  HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
